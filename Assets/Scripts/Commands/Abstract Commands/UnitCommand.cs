77273e4f5e53176e63dd5b3a81de6843b43a1af9
Ritesh Kumar Behera <ritz.rb50@gmail.com>
2026-03-20 10:04:51 +0530

    Command Interfaces

---
+++ Assets/Scripts/Commands/Abstract Commands/UnitCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public abstract class UnitCommand : ICommand
+{
+    public int ActorUnitID;
+    public int TargetUnitID;
+
+    public int ActorPlayerID;
+    public int TargetPlayerID;
+    public abstract void Execute();
+    public abstract bool WillHitTarget();
+}
