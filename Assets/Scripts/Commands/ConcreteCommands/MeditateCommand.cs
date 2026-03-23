a75165aa6c981a18774078de05d4cb9a67078322
Ritesh Kumar Behera <ritz.rb50@gmail.com>
2026-03-23 20:55:59 +0530

    Undo Method for all commands

--- Assets/Scripts/Commands/ConcreteCommands/MeditateCommand.cs
+++ Assets/Scripts/Commands/ConcreteCommands/MeditateCommand.cs
@@ -5,6 +5,7 @@ namespace Command.Commands
     public class MeditateCommand : UnitCommand
     {
         private bool willHitTarget;
+        private int previousMaxHealth;
         public MeditateCommand(CommandData commandData)
         {
             this.commandData = commandData;
@@ -14,5 +15,16 @@ namespace Command.Commands
         public override bool WillHitTarget() => true;
 
         public override void Execute() => GameService.Instance.ActionService.GetActionByType(CommandType.Meditate).PerformAction(actorUnit, targetUnit, willHitTarget);
+
+        public override void Undo()
+        {
+            if (willHitTarget)
+            {
+                var healthToReduce = targetUnit.CurrentMaxHealth - previousMaxHealth;
+                targetUnit.CurrentMaxHealth = previousMaxHealth;
+                targetUnit.TakeDamage(healthToReduce);
+            }
+            actorUnit.Owner.ResetCurrentActivePlayer();
+        }
     }
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -89,7 +89,19 @@ namespace Command.Player
         {
             units[activeUnitIndex].ResetUnitIndicator();
             activeUnitIndex--;
-            units[activeUnitIndex].StartUnitTurn();
+
+            while (activeUnitIndex >= 0)
+            {
+                if(!units[activeUnitIndex].IsAlive())
+                {
+                    activeUnitIndex--;
+                }
+                else
+                {
+                    units[activeUnitIndex].StartUnitTurn();
+                    break;
+                }
+            }
         }
 
         internal void ProcessUnitCommand(UnitCommand commandToProcess)
